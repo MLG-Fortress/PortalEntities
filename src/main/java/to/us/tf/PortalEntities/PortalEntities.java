9e144afdd7936848100f194f6390cf47ce562869
RoboMWM <RoboMWM@users.noreply.github.com>
2016-12-01 16:10:49 -0800

    note about potential area to make async

--- src/main/java/to/us/tf/PortalEntities/PortalEntities.java
+++ src/main/java/to/us/tf/PortalEntities/PortalEntities.java
@@ -5,7 +5,7 @@ import com.matejdro.bukkit.portalstick.PortalStick;
 import com.matejdro.bukkit.portalstick.events.PlayerPortalGunShootEvent;
 import de.V10lator.PortalStick.V10Location;
 import org.bukkit.Location;
-import org.bukkit.Material;
+ import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.World;
 import org.bukkit.block.Block;
@@ -15,7 +15,7 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
+ import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
@@ -25,7 +25,7 @@ import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.metadata.FixedMetadataValue;
-import org.bukkit.plugin.java.JavaPlugin;
+ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
@@ -35,7 +35,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-
+ 
 /**
  * Created by RoboMWM on 11/22/2016.
  * What a dumb plogen name, u come up wit a bettar 1
@@ -45,7 +45,7 @@ public class PortalEntities extends JavaPlugin implements Listener
     PortalStick portalStick;
     static PortalEntities instance; //boo
 
-    public void onEnable()
+     public void onEnable()
     {
         portalStick = (PortalStick)getServer().getPluginManager().getPlugin("PortalStick");
         getServer().getPluginManager().registerEvents(this, this);
@@ -55,7 +55,7 @@ public class PortalEntities extends JavaPlugin implements Listener
     /**
      * Use at beginning of every event handler
      * @return if world does not use portals
-     */
+      */
     public boolean disabledWorld(World world)
     {
         return portalStick.config.DisabledWorlds.contains(world.getName());
@@ -65,7 +65,7 @@ public class PortalEntities extends JavaPlugin implements Listener
     {
         return disabledWorld(location.getWorld());
     }
-
+ 
     public void smartTrackEntity(Entity entity)
     {
         if (entity.getType() == EntityType.PLAYER)
@@ -75,7 +75,7 @@ public class PortalEntities extends JavaPlugin implements Listener
             trackEntity(entity, false);
         }
     }
-
+ 
     public boolean isNearPortal(Location location)
     {
         World world = location.getWorld();
@@ -85,7 +85,7 @@ public class PortalEntities extends JavaPlugin implements Listener
         {
             Location portalLocation = convertV10Location(portal.inside[0]);
             try
-            {
+             {
                 if (location.distanceSquared(portalLocation) < 49) //7 blocks
                     return true;
                 continue;
@@ -95,7 +95,7 @@ public class PortalEntities extends JavaPlugin implements Listener
                 continue; //Just skip if there's an issue (null, not same world, etc.)
             }
         }
-        return false;
+         return false;
     }
 
     public Location convertV10Location(V10Location v10Location)
@@ -105,7 +105,7 @@ public class PortalEntities extends JavaPlugin implements Listener
 
     public void trackEntity(Entity entity)
     {
-        trackEntity(entity, true);
+         trackEntity(entity, true);
     }
 
     public void trackEntity(Entity entity, boolean check)
@@ -115,7 +115,7 @@ public class PortalEntities extends JavaPlugin implements Listener
             if (entity.getType() == EntityType.PLAYER)
                 return;
             if (entity.hasMetadata("TRACKED"))
-                return;
+                 return;
         }
         entity.setMetadata("TRACKED", new FixedMetadataValue(this, true));
         new BukkitRunnable()
@@ -125,7 +125,7 @@ public class PortalEntities extends JavaPlugin implements Listener
             Location previousLocation = entity.getLocation();
             public void run()
             {
-                if (entity.isDead() || !entity.isValid())
+                 if (entity.isDead() || !entity.isValid())
                 {
                     this.cancel();
                     return;
@@ -135,7 +135,7 @@ public class PortalEntities extends JavaPlugin implements Listener
                     if (entity.getLocation().distanceSquared(previousLocation) == 0)
                         return; //Don't call if it didn't move
                 }
-                catch (Exception e) //Somehow moved to another world, or otherwise erroring
+                 catch (Exception e) //Somehow moved to another world, or otherwise erroring
                 {
                     this.cancel();
                     entity.removeMetadata("TRACKED", PortalEntities.instance);
@@ -145,7 +145,7 @@ public class PortalEntities extends JavaPlugin implements Listener
                 Location to = portalStick.entityManager.onEntityMove(entity, previousLocation, entity.getLocation(), false);
                 if (to != null)
                 {
-                    entity.teleport(to);
+                     entity.teleport(to);
                     previousLocation = to;
                 }
                 else
@@ -155,7 +155,7 @@ public class PortalEntities extends JavaPlugin implements Listener
 
 //                if (--ticks < 0)
 //                {
-//                    entity.removeMetadata("TRACKED", PortalEntities.instance);
+ //                    entity.removeMetadata("TRACKED", PortalEntities.instance);
 //                    this.cancel();
 //                }
             }
@@ -165,7 +165,7 @@ public class PortalEntities extends JavaPlugin implements Listener
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     void onBlockFall(EntityChangeBlockEvent event)
     {
-        if (disabledWorld(event.getBlock().getLocation())) return;
+         if (disabledWorld(event.getBlock().getLocation())) return;
 
         if (event.getTo() == Material.AIR && event.getEntityType() == EntityType.FALLING_BLOCK)
             smartTrackEntity(event.getEntity());
@@ -175,7 +175,7 @@ public class PortalEntities extends JavaPlugin implements Listener
     void onItemSpawn(ItemSpawnEvent event)
     {
         if (disabledWorld(event.getLocation())) return;
-        smartTrackEntity(event.getEntity());
+         smartTrackEntity(event.getEntity());
     }
 
     //[22:07:03] RoboMWM: is EntityChangeBlockEvent supposed to fire whenever a block turns into a FallingBlock (or primedTNT entity)? Seems to only do so when a blockphysicsevent is called on the block (and it's supposed to fall) or if it's placed in creative mode.
@@ -185,7 +185,7 @@ public class PortalEntities extends JavaPlugin implements Listener
         if (disabledWorld(event.getBlock().getLocation())) return;
         Block block = event.getBlock();
         switch (block.getType())
-        {
+         {
             case SAND:
             case GRAVEL:
                 new BukkitRunnable()
@@ -195,7 +195,7 @@ public class PortalEntities extends JavaPlugin implements Listener
                         for (Entity entity : block.getLocation().getChunk().getEntities())
                         {
                             if (entity.getType() == EntityType.FALLING_BLOCK)
-                                smartTrackEntity(entity);
+                                 smartTrackEntity(entity);
                         }
                     }
                 }.runTaskLater(this, 1L);
@@ -205,7 +205,7 @@ public class PortalEntities extends JavaPlugin implements Listener
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     void onPlayerIgniteTNT(PlayerInteractEvent event)
     {
-        Block block = event.getClickedBlock();
+         Block block = event.getClickedBlock();
         if (disabledWorld(block.getLocation())) return;
 
         if (event.getAction() != Action.RIGHT_CLICK_BLOCK)
@@ -215,7 +215,7 @@ public class PortalEntities extends JavaPlugin implements Listener
             new BukkitRunnable()
             {
                 public void run()
-                {
+                 {
                     for (Entity entity : block.getLocation().getChunk().getEntities())
                     {
                         if (entity.getType() == EntityType.PRIMED_TNT)
@@ -225,7 +225,7 @@ public class PortalEntities extends JavaPlugin implements Listener
             }.runTaskLater(this, 1L);
         }
     }
-
+ 
     //We use a plugin to restore the old TNT behavior (ignite when destroyed)
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     void onPlayerIgniteTNTByBreaking(BlockBreakEvent event)
@@ -235,7 +235,7 @@ public class PortalEntities extends JavaPlugin implements Listener
 
         if (event.getBlock().getType() == Material.TNT)
         {
-            new BukkitRunnable()
+             new BukkitRunnable()
             {
                 public void run()
                 {
@@ -245,7 +245,7 @@ public class PortalEntities extends JavaPlugin implements Listener
                             smartTrackEntity(entity);
                     }
                 }
-            }.runTaskLater(this, 1L);
+             }.runTaskLater(this, 1L);
         }
     }
 
@@ -255,7 +255,7 @@ public class PortalEntities extends JavaPlugin implements Listener
         if (disabledWorld(event.getEntity().getLocation())) return;
 
         trackEntity(event.getEntity());
-    }
+     }
 
 
 // [22:53:46] RoboMWM: hmmm thoughts on making an "EntityMoveEvent" in a plugin via creating a runnable for each entity, checking location on each tick and firing event if distance != 0 ??????????
@@ -265,7 +265,7 @@ public class PortalEntities extends JavaPlugin implements Listener
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     void onMobSpawn(EntitySpawnEvent event)
     {
-        if (disabledWorld(event.getLocation())) return;
+         if (disabledWorld(event.getLocation())) return;
 
         trackEntity(event.getEntity());
     }
@@ -275,7 +275,7 @@ public class PortalEntities extends JavaPlugin implements Listener
     {
         if (disabledWorld(event.getWorld())) return;
 
-        if (event.isNewChunk())
+         if (event.isNewChunk())
             return;
         for (Entity entity : event.getChunk().getEntities())
         {
@@ -285,7 +285,7 @@ public class PortalEntities extends JavaPlugin implements Listener
             }
         }
     }
-
+ 
     /**
      * Extra feature
      */
@@ -324,7 +324,7 @@ public class PortalEntities extends JavaPlugin implements Listener
 
             public void run()
             {
-                world.spawnParticle(Particle.REDSTONE, vectorIterator.next().toLocation(world), 0, r, b, g, 0.0004D);
+                world.spawnParticle(Particle.REDSTONE, vectorIterator.next().toLocation(world), 0, r, b, g, 0.0004D); //toLocation can be rewritten async (List of Locations, then schedule sync task for spawnParticle)
                 if (!vectorIterator.hasNext())
                     this.cancel();
             }
