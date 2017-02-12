eebbb1b625c71dec962f3438a95329b6ea4ef2b8
RoboMWM <robomwm@users.noreply.github.com>
2017-02-12 12:41:26 -0800

    v1.6.1 nope, still purplish as ever.

--- src/main/java/to/us/tf/PortalEntities/PortalEntities.java
+++ src/main/java/to/us/tf/PortalEntities/PortalEntities.java
@@ -292,9 +292,9 @@ public class PortalEntities extends JavaPlugin implements Listener
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     void onPlayerShootsPortal(PlayerPortalGunShootEvent event)
     {
-        double red = Double.MIN_NORMAL;
-        double green = 0D;
-        double blue = 0D;
+        double red = Double.MIN_VALUE;
+        double green = Double.MIN_VALUE;
+        double blue = Double.MIN_VALUE;
         switch (event.getAction())
         {
             case LEFT_CLICK_AIR:
